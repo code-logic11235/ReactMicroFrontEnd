44019a1dcb50d54764f173b33eaeeca4dbd1f98f
Tai Pham <tai.pham1738@gmail.com>
2024-01-16 20:43:54 -0600

    added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -16,6 +16,7 @@ const prodConfig = {
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
+      filename: 'remoteEntry.js',
       exposes: { // declare which modules or pieces of code within a webpack bundle should be made available for consumption by other applications.
         './MarketingIndex': './src/bootstrap' // the name marketing /MarkingApp we will give them /src/bootstrap
       },
