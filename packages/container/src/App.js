35ae8d0ce60bdd062834b7111e17db5c786e2e80
Tai Pham <tai.pham1738@gmail.com>
2024-01-24 23:23:45 -0600

    fix css class name colision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
+import {
+    StylesProvider,
+    createGenerateClassName,
+    } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+
+// fixes css class name colision
+const generateClassName = createGenerateClassName({ 
+    productionPrefix: 'container', 
+    });
 
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header/>
-                <h1> hey from contianrer app.js</h1>
-                <MarketingApp/>
-            </div>
+            <StylesProvider>
+                    <div>
+                        <Header/>
+                        <h1> hey from contianrer app.js</h1>
+                        <MarketingApp/>
+                    </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
